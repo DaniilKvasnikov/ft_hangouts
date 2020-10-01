58ea23fa97a12138cbe06326eae416189f1fd5e7
rrhaenys <123yk123@mail.ru>
2020-10-01 15:05:31 +0300

    hsv settings

--- app/src/main/java/com/school21/ft_hangouts/SettingAppActivity.kt
+++ app/src/main/java/com/school21/ft_hangouts/SettingAppActivity.kt
@@ -1,9 +1,9 @@
 package com.school21.ft_hangouts
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingAppActivity : AppCompatActivity() {
 
